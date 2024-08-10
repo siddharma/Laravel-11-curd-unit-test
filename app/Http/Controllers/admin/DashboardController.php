d4473a216d49e84bc44d7cf2c4c9732a1226c83a
Dharmendra Sid <shid.dharma@gmail.com>
2024-08-10 16:13:18 +0530

    Laravel CURD application
    Created Laravel 11
    Created Admin & Client login

---
+++ app/Http/Controllers/admin/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    
+    public function index() {   
+
+         
+        return view("admin.dashboard");
+    }
+}
